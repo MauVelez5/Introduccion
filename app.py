c20c5ef78669071cec982611c6c2c8fa9cb8184b
MauVelez5 <118206119+MauVelez5@users.noreply.github.com>
2024-02-22 10:13:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title("Mi Primera App!!")#titulo o head de la pagina
 
 st.header("benveniue")
-st.write("l'importante n'est pas la chute, mais l'aterrisage)
+st.write("l'importante n'est pas la chute, mais l'aterrisage")
 image = Image.open('Medieval warrior.jpeg')
 
 st.image(image,caption='Interfaces multimodales')
