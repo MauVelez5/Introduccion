401183fd4bd40e4305302206230f727cd69806f5
MauVelez5 <118206119+MauVelez5@users.noreply.github.com>
2024-02-22 10:10:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st 
 from PIL import Image
 
-st.title("Mi Primera App!!")
+st.title("Mi Primera App!!")#titulo o head de la pagina
+
+st.header("benveniue")
+st.write("l'importante n'est pas la chute, mais l'aterrisage)
+image = Image.open('Interfaces multimodales')
+
+st.image(image,caption='Interfaces multimodales')
